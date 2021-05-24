f9e62f6973335c33005efcbbd999ac0fad6ed036
Wamika06 <73653192+Wamika06@users.noreply.github.com>
2021-05-24 19:59:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var p1, p2, asteroid1, asteroid2, asteroid3;
+var blast, blastimage, space, spaceimage;
+var spaceship, spaceshipimage, laserimage;
+var shoot = 0;
+var score = 0;
+var laser, asteroidGroup, laserGroup;
+
+function Preload(){
+    spaceimage = loadImage("space.png");
+    spaceshipimage = loadImage("spaceship.png");
+    laserimage = loadImage("laser.png");
+    
+}
